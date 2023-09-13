097ab2302c475d1091d2be4a52370b05311ef4a7
sifat <md.sifat.ur.rahman2702@gmail.com>
2023-09-13 19:39:59 +0600

    add molal

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+import tasksSlice from "./features/tasks/tasksSlice";
 
 const store = configureStore({
-    reducer:{},
+    reducer:{
+        tasks : tasksSlice,
+    },
 })
 export default store
