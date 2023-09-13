6ca9769b2df3426d762624fc183667b175e999c0
sifat <md.sifat.ur.rahman2702@gmail.com>
2023-09-13 19:27:30 +0600

    add slice

---
+++ src/redux/features/tasks/tasksSlice.js
@@ -0,0 +1,14 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const initialState={
+    tasks : []
+}
+ 
+const taskSlice = createSlice({
+    name:'tasksSlice',
+    initialState,
+    reducers:{},
+})
+
+export default taskSlice.reducer;
