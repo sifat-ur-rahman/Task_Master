6c25729d9e718070587f8bd83be6d83e8a1047d1
sifat <md.sifat.ur.rahman2702@gmail.com>
2023-09-14 08:30:04 +0600

    add form

--- src/components/tasks/AddTasksModal.jsx
+++ src/components/tasks/AddTasksModal.jsx
@@ -1,10 +1,30 @@
+import { useForm } from "react-hook-form"
 import Modal from "../Ui/Modal"
 
 
 function AddTasksModal({isOpen, setIsOpen}) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm()
+
+  const onSubmit = (data) => {
+    console.log(data)
+  }
   return (
     <Modal isOpen={isOpen} setIsOpen={setIsOpen} title={'sifat'}>
-            <p>lorem30</p>
+            <form onSubmit={handleSubmit(onSubmit)}>
+    
+      <input defaultValue="test" {...register("example")} />
+
+  
+      <input {...register("exampleRequired", { required: true })} />
+    
+      {errors.exampleRequired && <span>This field is required</span>}
+
+      <input type="submit" />
+    </form>
             </Modal>
   )
 }
